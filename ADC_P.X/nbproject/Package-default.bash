dec9f877db6ea3a3cd94ab407f49fde8fb5dcbfa
Hector Rodriguez <2017030465@upsin.edu.mx>
2020-09-15 10:38:32 -0600

    Empezamos ADC

--- ADC_P.X/nbproject/Package-default.bash
+++ ADC_P.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/vanilla.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=vanilla.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=vanilla.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/ADC_P.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=ADC_P.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=adcp.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/vanilla.x/bin
+makeDirectory ${TMPDIR}/adcp.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/vanilla.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/adcp.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/vanilla.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/adcp.x.tar *
 checkReturnCode
 
 # Cleanup
