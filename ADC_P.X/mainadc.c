85609a22986a7d54fa94d3501cab0e46831fe130
Hector Rodriguez <2017030465@upsin.edu.mx>
2020-09-16 12:46:39 -0600

    Configuramos pic para adc

---
+++ ADC_P.X/mainadc.c
@@ -0,0 +1,13 @@
+/*
+ * File:   mainadc.c
+ * Author: chiky
+ *
+ * Created on 16 de septiembre de 2020, 12:33 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
--- ADC_P.X/nbproject/Makefile-default.mk
+++ ADC_P.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=mainadc.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/mainadc.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/mainadc.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/mainadc.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=mainadc.c
 
 
 
@@ -94,7 +94,23 @@ MP_PROCESSOR_OPTION=18F46K22
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/mainadc.p1: mainadc.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/mainadc.p1.d 
+	@${RM} ${OBJECTDIR}/mainadc.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mainadc.p1 mainadc.c 
+	@-${MV} ${OBJECTDIR}/mainadc.d ${OBJECTDIR}/mainadc.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/mainadc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/mainadc.p1: mainadc.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/mainadc.p1.d 
+	@${RM} ${OBJECTDIR}/mainadc.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -memi=wordwrite -O0 -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx032 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/mainadc.p1 mainadc.c 
+	@-${MV} ${OBJECTDIR}/mainadc.d ${OBJECTDIR}/mainadc.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/mainadc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
@@ -136,11 +152,3 @@ endif
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r build/default
 	${RM} -r dist/default
-
-# Enable dependency checking
-.dep.inc: .depcheck-impl
-
-DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
-ifneq (${DEPFILES},)
-include ${DEPFILES}
-endif
